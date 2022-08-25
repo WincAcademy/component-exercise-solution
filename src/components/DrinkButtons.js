1ac5661a7535c467bdbc881449247a646f8335da
Mark van der Laan <mrkvdl@gmail.com>
2022-08-25 15:03:54 +0200

    add props exercise solution

--- src/components/DrinkButtons.js
+++ src/components/DrinkButtons.js
@@ -1,12 +1,14 @@
 import './DrinkButtons.css';
 
-export const DrinkButtons = () => {
+export const DrinkButtons = ({ drinkOne, drinkTwo }) => {
 	return (
 		<>
-			<h2>Would you like tea or coffee?</h2>
+			<h2>
+				Would you like {drinkOne} or {drinkTwo}?
+			</h2>
 			<div className="Button-group">
-				<button className="Button">Tea</button>
-				<button className="Button">Coffee</button>
+				<button className="Button">{drinkOne}</button>
+				<button className="Button">{drinkTwo}</button>
 			</div>
 		</>
 	);
